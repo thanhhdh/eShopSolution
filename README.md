520e7618df7ef986375133603159c8dc7174d418
Ho Dac Hoang Thanh <hdhthanh123@gmail.com>
2023-06-12 17:01:38 +0700

    Add Entity

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 ## Technologies
 - ASP.NET
 - Entity Framework core
+## Install packages
+- microsoft.EntityFrameworkCore.Design
+- microsoft.EntityFrameworkCore.Tool
+- microsoft.EntityFrameworkCore.SqlSever
+- 
 # How to configuration and run
 
---
+++ eShopSolution.Data/Entities/Promotion.cs
@@ -0,0 +1,23 @@
+﻿using eShopSolution.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Promotion
+    {
+        public int Id { get; set; }
+        public DateTime FromDate { get; set; } 
+        public DateTime ToDate { get; set; }
+        public bool ApplyForAll { get; set; }
+        public int? DiscountPercent { get; set; }
+        public decimal? DiscountAmount { get; set; }
+        public string ProductIds { get; set; }
+        public string ProductCategoryIds { get; set; }
+        public Status Status { set; get; }
+        public string Name { get; set; }
+    }
+}
