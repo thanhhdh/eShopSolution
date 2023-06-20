0a9b83aa4ddd957dc39ef059772568829562dc67
Ho Dac Hoang Thanh <hdhthanh123@gmail.com>
2023-06-20 16:59:27 +0700

    Admin Page and cookie authenticate

---
+++ eShopSolution.AdminApp/Program.cs
@@ -0,0 +1,54 @@
+using eShopSolution.AdminApp.Services;
+using eShopSolution.ViewModels.System.Users;
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var mvcBuilder = builder.Services.AddRazorPages();
+
+if (builder.Environment.IsDevelopment())
+{
+    mvcBuilder.AddRazorRuntimeCompilation();
+}
+builder.Services.AddHttpClient();
+
+builder.Services.AddTransient<IUserApiClient, UserApiClient>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+{
+    options.LoginPath = "/User/Login/";
+    options.LogoutPath = "/User/Forbidden/";
+});
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseAuthentication();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
