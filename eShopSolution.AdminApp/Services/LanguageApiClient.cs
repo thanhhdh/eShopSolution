48b1cf89d1aea999fc9d60113123a32e6ea33554
Ho Dac Hoang Thanh <hdhthanh123@gmail.com>
2023-07-06 16:21:17 +0700

    Filter dropdown

--- eShopSolution.AdminApp/Services/LanguageApiClient.cs
+++ eShopSolution.AdminApp/Services/LanguageApiClient.cs
@@ -8,14 +8,12 @@ namespace eShopSolution.AdminApp.Services
 {
     public class LanguageApiClient : BaseApiClient, ILanguageApiClient
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly IConfiguration _configuration;
-        private readonly IHttpContextAccessor _httpContextAccessor;
+        
         public LanguageApiClient(
             IHttpClientFactory httpClientFactory,
-            IConfiguration configuration,
-            IHttpContextAccessor httpContextAccessor
-            ) : base (httpClientFactory, configuration, httpContextAccessor){}
+            IHttpContextAccessor httpContextAccessor,
+            IConfiguration configuration
+            ) : base (httpClientFactory, httpContextAccessor, configuration) {}
         public async Task<ApiResult<List<LanguageVm>>> GetAll()
         {
             return await GetAsync<ApiResult<List<LanguageVm>>>("/api/languages");
