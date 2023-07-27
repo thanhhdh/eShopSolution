1b1deefc877a57d15effe7485eb5a48cd4b3fa91
Ho Dac Hoang Thanh <hdhthanh123@gmail.com>
2023-07-27 18:58:41 +0700

    Show Category and Detail Productt

--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -9,9 +9,9 @@ namespace eShopSolution.Data.Entities
     public class Product
     {
         public int Id { set; get; }
-        public decimal Price { set; get; }
-        public decimal OriginalPrice { set; get; }
-        public int Stock { set; get; }
+        public decimal? Price { set; get; }
+        public decimal? OriginalPrice { set; get; }
+        public int? Stock { set; get; }
         public int ViewCount { set; get; }
         public DateTime DateCreated { set; get; }
         public bool? IsFeatured { get; set; }
