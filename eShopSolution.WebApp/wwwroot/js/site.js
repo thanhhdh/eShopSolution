e332dc38a7b5e801fda7c9ddf7437491e2ccc548
Ho Dac Hoang Thanh <hdhthanh123@gmail.com>
2023-08-03 18:08:22 +0700

    List Cart

--- eShopSolution.WebApp/wwwroot/js/site.js
+++ eShopSolution.WebApp/wwwroot/js/site.js
@@ -1,4 +1,39 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var siteController = function () {
+    this.initialize = function () {
+        registerEvents()
+        loadCart();
+    }
+    function loadCart() {
+        const culture = $('#hidCulture').val();
+        $.ajax({
+            type: "GET",
+            url: "/" + culture + "/cart/GetListItem",
+            success: function (res) {
+                $('#lbl_number_item').text(res.length);
+            },
+        })
+    }
+    function registerEvents() {
+        $('body').on('click', '.btn-add-cart', function (e) {
+            e.preventDefault();
+            const id = $(this).data('id');
+            const culture = $('#hidCulture').val();
+            $.ajax({
+                type: "POST",
+                url: "/" + culture + "/cart/AddToCart",
+                data: {
+                    id: id,
+                    languageId: culture
+                },
+                success: function (res) {
+                    $('#lbl_number_item').text(res.length);
+                },
+            })
+        })
 
-// Write your JavaScript code.
+    }
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
