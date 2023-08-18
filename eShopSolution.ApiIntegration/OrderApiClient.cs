5830123eae41c3d1fb0b62e19c95b87012e34a29
Ho Dac Hoang Thanh <hdhthanh123@gmail.com>
2023-08-18 15:35:28 +0700

    commit

--- eShopSolution.ApiIntegration/OrderApiClient.cs
+++ eShopSolution.ApiIntegration/OrderApiClient.cs
@@ -10,7 +10,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
-using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 
